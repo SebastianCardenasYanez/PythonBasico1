63d04be43f95750fc730cd34ccaa541c8a661df4
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-02-23 14:38:29 -0500

    algoritmo para nummero mayor

---
+++ NumeroMayor.py
@@ -0,0 +1,9 @@
+num1 = input("ingrese munero 1: ")
+num2 = input("ingrese munero 2: ")
+num3 = input("ingrese munero 3: ")
+if num1 > num2 and num1 > num3 :
+    print ("el numero mayor es: ", num1)
+elif num2 > num1 and num2 > num3 :
+    print ("el numero mayor es: ", num2) 
+elif num3 > num1 and num3 > num2 :
+    print ("el numero mayor es: ", num3)
