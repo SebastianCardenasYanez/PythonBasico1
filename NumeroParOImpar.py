f4a80e9f19c8d3d7d8192f77409a385f0787b4ba
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-02-23 14:46:05 -0500

    .

--- NumeroParOImpar.py
+++ NumeroParOImpar.py
@@ -3,4 +3,4 @@ if num == 0 :
     print ("no es par o impar: ", num)
 elif num % 2 == 0 :
     print("el numero es par: ", num)
-else : print("el numero es impar", num),
+else : print("el numero es impar", num)
