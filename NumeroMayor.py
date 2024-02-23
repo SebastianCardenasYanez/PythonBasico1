3e537f85a1f132e26b890b46cc6ec3dfc8561ade
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-02-23 14:31:52 -0500

    Algoritmo de numero par o impar

--- NumeroMayor.py
+++
@@ -1,9 +0,0 @@
-num1 = input("ingrese munero 1: ")
-num2 = input("ingrese munero 2: ")
-num3 = input("ingrese munero 3: ")
-if num1 > num2 and num1 > num3 :
-    print ("el numero mayor es: ", num1)
-elif num2 > num1 :
-    print ("el numero mayor es: ", num2) 
-elif num3 > num1 :
-    print ("el numero mayor es: ", num3)
---
+++ NumeroParOImpar.py
@@ -0,0 +1,6 @@
+num = int (input ("ingrese numero: "))
+if num == 0 :
+    print ("no es par o impar: ", num)
+elif num % 2 == 0 :
+    print("el numero es par: ", num)
+else : print("el numero es impar", num)
